f9c8b4e2b297934e67fa24a75809e760223de06e
Angela Shen <angelayshen@gmail.com>
2023-06-05 16:31:50 -0400

    add homepage

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom'
 function NavBar() {
     return ( 
         <>
+          <NavLink to='/'>Homepage</NavLink>
           <NavLink to='/search'>Search</NavLink>
           <NavLink to='/newTweet'>New Tweet</NavLink>
           <NavLink to='/filter'>Filter</NavLink>
